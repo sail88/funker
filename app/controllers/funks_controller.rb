73064bd7ad9a9e1896280d785697f53581c7ec53
Steven Gordon <steven@SRG-MBP.local>
2015-05-01 21:23:28 -0700

    Added some authorization & navigation

--- app/controllers/funks_controller.rb
+++ app/controllers/funks_controller.rb
@@ -1,25 +1,34 @@
 class FunksController < ApplicationController
 
   def index
-  	#
-  	@funks = Funk.all
+    logged_in?
+    # show all the Funks of the user whose id is in the URL
+    whose_funks = params[:user_id].to_i
+    
+    # NEED TO CHECK THAT CURRENT USER HAS ACCESS -- either is the same user or a follower
+    if whose_funks == current_user.id
+      @funks = Funk.where({user_id: session[:user_id]})
+    else
+      redirect_to "/login"
+    end 
   end
 
   def new
-  	@user = @current_user
+    logged_in?
+  	@user = current_user
   	@funk = Funk.new
     #byebug
   end
 
   def create
-  	@user = @current_user
+    logged_in?
+  	@user = current_user
   	funk_params = params.require(:funk).permit(:content)
     #byebug
   	#new_content = funk_params[:content]
   	@funk = @user.funks.create(funk_params) #this is one step way of ASSOCIATING and CREATING a new funk
 
   	redirect_to "/"
-
   end
 
   def show
